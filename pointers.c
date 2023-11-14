fd95e7de6623e5e7af284b6299c9864d285463ed
subham1265 <146015047+subham1265@users.noreply.github.com>
2023-11-14 21:15:03 +0530

    swap numbers
    swaping of numbers using a pointer in a c language

--- pointers.c
+++ pointers.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-int main() {
-  int num = 10;
-  int *ptr = &num; // ptr holds the address of num
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
 
-  printf("The value of num is: %d\n", num);
-  printf("The address of num is: %p\n", &num);
-  printf("The value of ptr is: %p\n", ptr);
-  printf("The value stored at the address pointed to by ptr is: %d\n", *ptr);
+int main() {
+  int x = 10;
+  int y = 20;
+
+  printf("Before swapping: x = %d, y = %d\n", x, y);
+  swap(&x, &y);
+  printf("After swapping: x = %d, y = %d\n", x, y);
 
   return 0;
 }
